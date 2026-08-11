561897bb40f4da2cdeba10549babbdfa1448356e
Md Arman <xsmartarman123@gmail.com>
2026-08-11 23:11:34 +0530

    add backend cloud url

--- Client/Frontend/src/api/Axios.jsx
+++ Client/Frontend/src/api/Axios.jsx
@@ -1,19 +1,43 @@
+// import axios from "axios";
+// export const authUrl = axios.create({
+//   baseURL: "http://localhost:4400/api/user",
+//   withCredentials: true,
+// });
+// export const profileUrl = axios.create({
+//   baseURL: "http://localhost:4400/api/profile",
+//   withCredentials: true,
+// });
+
+// export const postUrl = axios.create({
+//   baseURL: "http://localhost:4400/api/post",
+//   withCredentials: true,
+// });
+
+// export const messageUrl = axios.create({
+//   baseURL: "http://localhost:4400/api/message",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
-export const authUrl = axios.create({
-  baseURL: "http://localhost:4400/api/user",
+
+const API_URL = "https://socail-media-4.onrender.com";
+
+export const userUrl = axios.create({
+  baseURL: `${API_URL}/api/user`,
   withCredentials: true,
 });
+
 export const profileUrl = axios.create({
-  baseURL: "http://localhost:4400/api/profile",
+  baseURL: `${API_URL}/api/profile`,
   withCredentials: true,
 });
 
 export const postUrl = axios.create({
-  baseURL: "http://localhost:4400/api/post",
+  baseURL: `${API_URL}/api/post`,
   withCredentials: true,
 });
 
 export const messageUrl = axios.create({
-  baseURL: "http://localhost:4400/api/message",
+  baseURL: `${API_URL}/api/message`,
   withCredentials: true,
 });
