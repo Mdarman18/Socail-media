52d7e2f394892136c945c399a5d1fa93352cf0e3
Md Arman <xsmartarman123@gmail.com>
2026-08-11 22:30:26 +0530

    fix server.js

--- Server/server.js
+++ Server/server.js
@@ -22,7 +22,7 @@ import { app, server } from "./src/socket/socekt.js";
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://socail-media-cyan.vercel.app"],
     credentials: true,
   }),
 );
@@ -52,6 +52,6 @@ app.use("/auth", authRoute);
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running On http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server is running On http://localhost:${PORT}`);
+// });
