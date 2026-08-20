400b53304dba3862f0c7fb9bd7b5a66beb69f43c
Md Arman <xsmartarman123@gmail.com>
2026-08-20 21:23:49 +0530

    fix some server issue

--- Client/Frontend/src/pages/Message.jsx
+++ Client/Frontend/src/pages/Message.jsx
@@ -4,6 +4,7 @@ import { messageUrl, profileUrl } from "../api/Axios";
 import { useSocketContext } from "../context/SocketContext";
 import { addMessage, setMessage } from "../store/Message";
 import { FaArrowLeft } from "react-icons/fa";
+import { persistor } from "../store/store";
 
 const BasicOnlineCount = () => {
   const dispatch = useDispatch();
@@ -154,7 +155,10 @@ const BasicOnlineCount = () => {
     const date = new Date(timestamp);
     return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
   };
-  console.log(allMessages);
+  allMessages.map((msg) => {
+    console.log("timing", msg?.createdAt); // Yeh sahi tarika hai
+    // ...
+  });
 
   return (
     <div className="flex flex-col md:flex-row h-[calc(100vh-100px)] w-full overflow-hidden bg-slate-50">
@@ -308,27 +312,27 @@ const BasicOnlineCount = () => {
                   return (
                     <div
                       key={msg._id}
-                      className={`flex ${
-                        isMe ? "justify-end" : "justify-start"
-                      }`}
+                      className={`flex ${isMe ? "justify-end" : "justify-start"}`}
                     >
                       <div
-                        className={`max-w-[80%] md:max-w-xs rounded-xl px-4 py-2 ${
+                        className={`max-w-[80%] md:max-w-xs rounded-xl px-3 py-1.5 ${
                           isMe
                             ? "bg-violet-600 text-white"
                             : "border bg-white text-slate-700 shadow-sm"
                         }`}
                       >
-                        <p className="text-sm wrap-break-word">{msg.message}</p>
+                        <p className="text-sm break-words leading-snug">
+                          {msg.message}
+                        </p>
 
                         {/* Message Timestamp */}
-                        <div
-                          className={`text-[10px] mt-1 text-right ${
+                        <span
+                          className={`block text-right text-[10px] mt-0.5 ${
                             isMe ? "text-violet-200" : "text-slate-400"
                           }`}
                         >
                           {formatTime(msg.createdAt || msg.updatedAt)}
-                        </div>
+                        </span>
                       </div>
                     </div>
                   );
